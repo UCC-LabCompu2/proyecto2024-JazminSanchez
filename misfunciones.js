a21d78bd3d4397f3ed2ce58e0225e872e2d7c568
jazminmorenasanchez <2321544@ucc.edu.ar>
2024-06-08 12:34:09 -0300

    modifique la funcion verificar y seguir para que esten juntas

--- misfunciones.js
+++ misfunciones.js
@@ -1,46 +1,40 @@
-function verificarRespuesta() {
-    // La respuesta correcta
-    const respuestaCorrecta =["alohas", "zumba"];
-    // Obtener el valor ingresado por el usuario
-    const respuestaUsuario = document.getElementById("respuesta").value.trim().toLowerCase();
-    // Elemento donde se mostrará el resultado
-    const resultado = document.getElementById("resultado");
-
-    // Verificar si la respuesta es correcta
-    if (respuestaCorrecta.includes(respuestaUsuario)) {
-        resultado.textContent = "¡Correcto! ¡Bien hecho!";
-        resultado.style.color = "green";
-    } else {
-        resultado.textContent = "Incorrecto, intenta de nuevo.";
-        resultado.style.color = "red";
-    }
-}
 function saltarAdivinanza() {
     if (confirm("¿Estás seguro de que deseas saltar la adivinanza? PERDERAS 10 PUNTOS")) {
         const paginaactual = window.location.pathname;
-        if (paginaactual.endsWith("jugar.html")){
+        if (paginaactual.endsWith("Jugar.html")){
             window.location.href = "jugar2.html";
         }else {
-            window.location.href = "jugar.html";
+            window.location.href = "Jugar.html";
         }
     } else {
         alert("¡Se que tu puedes!");
     }
 }
 function ayuda() {
-    alert("Es 1 palabra, muy conocida en Hawai");
+    const paginaactual = window.location.pathname;
+    if (paginaactual.endsWith("Jugar.html")){
+        alert("Es 1 palabra, muy conocida en Hawai");
+    }else {
+        alert("Clase de ejercicios aeróbicos que involucra baile y música.");
+    }
 }
 function seguir() {
-    const respuestascorrectas = ["alohas", "zumba"];
-    const respuesta = document.getElementById("respuesta").value.trim().toLowerCase(); // Obtener la respuesta ingresada por el usuario
-        if (respuestascorrectas.includes(respuesta)) { // Verificar si la respuesta es correcta
+    // La respuesta correcta
+    const respuestaCorrecta =["aloha", "zumba"];
+    // Obtener el valor ingresado por el usuario
+    const respuestaUsuario = document.getElementById("respuesta").value.trim().toLowerCase();
+    // Elemento donde se mostrará el resultado
+    const resultado = document.getElementById("resultado");
+
+        if (respuestaCorrecta.includes(respuestaUsuario)) { // Verificar si la respuesta es correcta
             const paginaactual = window.location.pathname;
-            if (paginaactual.endsWith("jugar.html")){
+            if (paginaactual.endsWith("Jugar.html")){
                 window.location.href = "jugar2.html";
             }else {
-                window.location.href = "jugar.html";
+                window.location.href = "Jugar.html";
             }
         } else {
-            alert("Debes completar primero esta adivinanza antes de avanzar.");
+            resultado.textContent = "Incorrecto, intenta de nuevo.";
+            resultado.style.color = "red";
         }
 }
