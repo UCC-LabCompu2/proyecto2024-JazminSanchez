5871d81e160bfdfce62ea34da957f7f0b30a65a9
jazminmorenasanchez <2321544@ucc.edu.ar>
2024-06-09 17:13:52 -0300

    agregue funcion minipaint

--- misfunciones.js
+++ misfunciones.js
@@ -80,3 +80,29 @@ function calculardiv() {
     num2 = document.getElementsByName("div_num2")[0].value;
     document.getElementsByName("div_total")[0].innerHTML = Number(num1) / Number(num2);
 }
+
+//canva
+var bandera;
+function dibujar(event) {
+    var canvas = document.getElementById("canvasadibujar");
+    var ctx = canvas.getContext("2d");
+
+    var rect = canvas.getBoundingClientRect();
+    var posx = event.clientX - rect.left;
+    var posy = event.clientY - rect.top;
+    console.log(posx, posy);
+
+    canvas.onmousedown = function(){bandera = true};
+    canvas.onmouseup = function(){bandera = false};
+
+    if(bandera) {
+        ctx.fillRect(posx, posy, 5, 5);
+        ctx.fill;
+    }
+}
+function limpiarcanvas() {
+    var canvas = document.getElementById("canvasadibujar");
+    var ctx = canvas.getContext("2d");
+
+    canvas.width = canvas.width;
+}
