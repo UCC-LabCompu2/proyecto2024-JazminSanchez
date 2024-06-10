b797f98a59bd40a014097abd2850bef1bb3f73e2
jazminmorenasanchez <2321544@ucc.edu.ar>
2024-06-09 23:33:56 -0300

    se mejoro funcion clculadora

--- misfunciones.js
+++ misfunciones.js
@@ -44,27 +44,53 @@ function calcularsuma() {
     var num1, num2;
     num1 = document.getElementsByName("sum_num1")[0].value;
     num2 = document.getElementsByName("sum_num2")[0].value;
-    document.getElementsByName("sum_total")[0].innerHTML = Number(num1) + Number(num2);
+
+    if(isNaN(num1) || isNaN(num2)) {
+        alert("se ingreso un valor invalido");
+        document.getElementsByName("sum_num1")[0].value = "";
+        document.getElementsByName("sum_num2")[0].value = "";
+    }else{
+        document.getElementsByName("sum_total")[0].innerHTML = Number(num1) + Number(num2);
+    }
 }
 function calcularresta() {
     var num1, num2;
     num1 = document.getElementsByName("res_num1")[0].value;
     num2 = document.getElementsByName("res_num2")[0].value;
-    document.getElementsByName("res_total")[0].innerHTML = Number(num1) - Number(num2);
+
+    if(isNaN(num1) || isNaN(num2)) {
+        alert("se ingreso un valor invalido");
+        document.getElementsByName("res_num1")[0].value = "";
+        document.getElementsByName("res_num2")[0].value = "";
+    }else {
+        document.getElementsByName("res_total")[0].innerHTML = Number(num1) - Number(num2);
+    }
 }
 
 function calcularmul() {
     var num1, num2;
     num1 = document.getElementsByName("mul_num1")[0].value;
     num2 = document.getElementsByName("mul_num2")[0].value;
-    document.getElementsByName("mul_total")[0].innerHTML = Number(num1) * Number(num2);
+    if(isNaN(num1) || isNaN(num2)) {
+        alert("se ingreso un valor invalido");
+        document.getElementsByName("mul_num1")[0].value = "";
+        document.getElementsByName("mul_num2")[0].value = "";
+    }else {
+        document.getElementsByName("mul_total")[0].innerHTML = Number(num1) * Number(num2);
+    }
 }
 
 function calculardiv() {
     var num1, num2;
     num1 = document.getElementsByName("div_num1")[0].value;
     num2 = document.getElementsByName("div_num2")[0].value;
-    document.getElementsByName("div_total")[0].innerHTML = Number(num1) / Number(num2);
+    if(isNaN(num1) || isNaN(num2)) {
+        alert("se ingreso un valor invalido");
+        document.getElementsByName("div_num1")[0].value = "";
+        document.getElementsByName("div_num2")[0].value = "";
+    }else {
+        document.getElementsByName("div_total")[0].innerHTML = Number(num1) / Number(num2);
+    }
 }
 
 //canva
